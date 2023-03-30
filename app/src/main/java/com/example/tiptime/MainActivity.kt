d6049f53f3e812b72c7478403acd014d26878dab
Gustavo <gustavu0720@gmail.com>
2023-03-29 22:44:10 -0300

    finalização

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener{ calculateTip()}
+
+
+
+    }
+    fun calculateTip() {
+        val stringInTextField = binding.preco.text.toString()
+        val cost = stringInTextField.toDouble()
+
+        val selectedId = binding.grupo.checkedRadioButtonId
+
+        val tipPercentage = when (selectedId) {
+            R.id.percentual20 -> 0.20
+            R.id.percentual18 -> 0.18
+            else -> 0.15
+        }
+
+        var tip = tipPercentage * cost
+        val roundUp = binding.switch1.isChecked
+        if (roundUp) {
+            tip = kotlin.math.ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.valor,formattedTip)
+
     }
 }
